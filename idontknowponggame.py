a5d75dc3532ed7944b248efee370043a929288ae
alexgaming9324yt <alexrafahl91@gmail.com>
2026-05-30 14:40:19 +0300

    .py file you can modify or go to releases for the exe

---
+++ idontknowponggame.py
@@ -0,0 +1,43 @@
+import pygame 
+
+pygame.init()
+res = pygame.display.set_mode((800, 400))
+ticks = pygame.time.Clock()
+
+p1 = pygame.Rect(20, 150, 10, 80)
+p2 = pygame.Rect(770, 150, 10, 80)
+BALL = pygame.Rect(390, 190, 20, 20)
+
+vx, vy = 5, 5 
+
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+    k = pygame.key.get_pressed()
+
+    if k[pygame.K_w]: p1.y -= 5
+    if k[pygame.K_s]: p1.y += 5
+    if k[pygame.K_i]: p2.y -= 5
+    if k[pygame.K_k]: p2.y += 5
+
+    BALL.x += vx
+    BALL.y += vy
+
+    if BALL.top <= 0 or BALL.bottom >= 400:
+        vy *= -1
+
+    if BALL.colliderect(p1) or BALL.colliderect(p2):
+        vx *= -1
+
+    if BALL.left <= 0 or BALL.right >= 800:
+        BALL.center = (400, 200)
+
+    res.fill((0, 0, 0))
+    pygame.draw.rect(res, (255, 255, 255), p1)
+    pygame.draw.rect(res, (255, 255, 255), p2)
+    pygame.draw.ellipse(res, (255, 255, 255), BALL)
+
+    pygame.display.flip()
+    ticks.tick(60)
